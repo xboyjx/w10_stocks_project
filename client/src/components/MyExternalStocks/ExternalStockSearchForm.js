ff638fa0c49aaf3d5cd4448d83944ace947e1b16
burgic <ceburgin@gmail.com>
2022-04-25 14:53:01 +0100

    search states

--- client/src/components/MyExternalStocks/ExternalStockSearchForm.js
+++ client/src/components/MyExternalStocks/ExternalStockSearchForm.js
@@ -1,7 +1,20 @@
 
 
-const ExternalStockSearchForm = () => {
-    <h2>External Stock Search Form</h2>
+const ExternalStockSearchForm = ({handleSearchTerm}) => {
+    
+    const handleChange = (event) => {
+        handleSearchTerm (event.target.value)
+    }
+
+    return (
+        <>
+        <h2>External Stock Search Form</h2>
+
+        <input type = "text" placeholder= "Search" name="searchedTerm" onChange={handleChange} />
+
+        </>
+    )
+
 }
 
 export default ExternalStockSearchForm
--- client/src/containers/ExternalStocksContainer.js
+++ client/src/containers/ExternalStocksContainer.js
@@ -4,13 +4,29 @@ import { useState, useEffect } from 'react';
 
 const ExternalStocksContainer = () => {
 
-    const [searchedTerm, setSearchedTerm] = useState("")
+    const [searchedTerm, setSearchedTerm] = useState(null)
+    const [searchedResults, setSearchedResults] = useState([])
     
+    useEffect (() => {
+        if (searchedTerm !== null) {
+            fetchMySearchResults()
+        } 
+    }, [searchedTerm])
+
+    const fetchMySearchResults = function(searchValues){
+        fetch(`https://api.twelvedata.com/symbol_search?symbol=${searchedTerm}`)
+        .then(res => res.json())
+        .then(data => setSearchedResults(data))
+    }
+
+    const handleSearchTerm = (search) => {
+        setSearchedTerm(search)
+    } 
 
     return (
         <>
         <h1>Potato</h1>
-        <ExternalStockSearchForm />
+        <ExternalStockSearchForm handleSearchTerm={handleSearchTerm} />
         <ExternalStockDetail />
         </>
     )
