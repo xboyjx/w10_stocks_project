722a6fc3cf55b15f7e248661f384b749ae6ec251
burgic <ceburgin@gmail.com>
2022-04-25 15:28:36 +0100

    API dictionary

--- client/src/containers/ExternalStocksContainer.js
+++ client/src/containers/ExternalStocksContainer.js
@@ -1,5 +1,6 @@
 import ExternalStockSearchForm from "../components/MyExternalStocks/ExternalStockSearchForm";
 import ExternalStockDetail from "../components/MyExternalStocks/ExternalStockDetail";
+import SearchResultsList from "../components/MyExternalStocks/SearchResultsList";
 import { useState, useEffect } from 'react';
 
 const ExternalStocksContainer = () => {
@@ -16,8 +17,9 @@ const ExternalStocksContainer = () => {
     const fetchMySearchResults = function(searchValues){
         fetch(`https://api.twelvedata.com/symbol_search?symbol=${searchedTerm}`)
         .then(res => res.json())
-        .then(data => setSearchedResults(data))
+        .then(data => setSearchedResults(data.data))
     }
+
 
     const handleSearchTerm = (search) => {
         setSearchedTerm(search)
@@ -27,6 +29,7 @@ const ExternalStocksContainer = () => {
         <>
         <h1>Potato</h1>
         <ExternalStockSearchForm handleSearchTerm={handleSearchTerm} />
+    {searchedResults.length > 0? <SearchResultsList searchedResults={searchedResults}/>: null }
         <ExternalStockDetail />
         </>
     )
