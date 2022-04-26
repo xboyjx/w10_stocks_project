c3d00c739350a805c24d0d87d2ac3ef77ea790d4
burgic <ceburgin@gmail.com>
2022-04-26 16:21:06 +0100

    insert buttons and times

--- client/src/components/MyStocks/MyStockItemsGraph.js
+++ client/src/components/MyStocks/MyStockItemsGraph.js
@@ -5,13 +5,35 @@ import HighchartsReact from 'highcharts-react-official';
 const MyStockItemsGraph = ({selectedStock}) => {
 
     const [data, setData] = useState(null)
+    const [interval, setInterval] = useState("1month")
+    const [ticker, setTicker] = useState(selectedStock.meta.symbol)
+    console.log(selectedStock.stock)
+    const [myStockData, setMyStockData] = useState(null)
+
+    const handleClick = (event) => {
+        setInterval(event.target.value)
+    }
+
+    const keys = ["b9807276d5104b33b92d92ff766ab531", "45bcd3cd4e5744298c4fdb0dcd6a1249", "f95ae7b120094694aa29d206c16751f4", "4d3df07bd71d4d198ec7b0071f70c982", "8603921c05d4466f92b11e6ebb617a1b"]
+    const randKey = keys[Math.floor(Math.random() * keys.length)]
+
+    const fetchMyStockData = function(ticker){
+        fetch(`https://api.twelvedata.com/time_series?symbol=${ticker}&interval=${interval}&apikey=${randKey}`)
+        .then(res => res.json())
+        .then(data => setMyStockData(data))}
+
+        useEffect(() => {
+            fetchMyStockData(ticker)
+        }, [selectedStock, interval])
 
     useEffect(() => {
-        makeChart()
-    }, [selectedStock] )
+        if (myStockData !== null ){
+            makeChart()
+        } 
+        }, [myStockData] )
 
     const makeChart = () => {
-        const stockDataValuesList = selectedStock.values.map((value, index) => {
+        const stockDataValuesList = myStockData.values.map((value, index) => {
             return parseInt(value.close)
         }) 
         let reversedArray =[]
@@ -21,7 +43,7 @@ const MyStockItemsGraph = ({selectedStock}) => {
         setData(reversedArray)
     }
     
-    // const stockDataDatesList = selectedStock.values.map((value, index) => {
+    // const stockDataDatesList = myStockData.values.map((value, index) => {
     //     return value.close
     // })
 
@@ -53,6 +75,10 @@ const MyStockItemsGraph = ({selectedStock}) => {
                 <h3>STOCK: {selectedStock.meta.symbol} : EXCHANGE: {selectedStock.meta.exchange}</h3>
                 <h4>{selectedStock.meta.interval}</h4>
                 <h4>CURRENCY: {selectedStock.meta.currency}</h4>
+                <button onClick={handleClick} value="1h">Hourly</button>
+                <button onClick={handleClick} value="1day">Daily</button>
+                <button onClick={handleClick} value="1week">Weekly</button>
+                <button onClick={handleClick} value="1month">Monthly</button>
             </div>
             {data !== null ? <HighchartsReact highcharts={Highcharts} constructor-type={'stockChart'} options={options}/> :null}
         </div>
